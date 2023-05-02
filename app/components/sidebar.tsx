8fce6ca84530de36fdb054e4f124eea360c291cc
lcxwy <lcxwyc@live.com>
2023-05-02 19:13:26 +0800

    dd

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -96,9 +96,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>阿宗的GPT</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          尽情提问吧~
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
